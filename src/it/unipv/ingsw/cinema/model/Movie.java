6fe651b7954ec325d2437fffc914d67486fddbbd
AmatoFrancesco99 <francesco.amato01@universitadipavia.it>
2021-04-20 18:04:21 +0200

    Update some class

--- src/it/unipv/ingsw/cinema/model/Movie.java
+++ src/it/unipv/ingsw/cinema/model/Movie.java
@@ -1,5 +1,19 @@
 package it.unipv.ingsw.cinema.model;
 
+import java.util.Date;
+
 public class Movie {
 
+	private String name,cast,producer;
+	private int duration, minAgeSee;
+	private Date releaseDate;
+	
+	public Movie (String name, String cast, String producer, int duration, int minAgeSee, Date releaseDate) {
+		this.name=name;
+		this.cast=cast;
+		this.producer=producer;
+		this.duration=duration;
+		this.minAgeSee=minAgeSee;
+		this.releaseDate=releaseDate;
+	}
 }
--- src/it/unipv/ingsw/cinema/model/cinema/Room.java
+++ src/it/unipv/ingsw/cinema/model/cinema/Room.java
@@ -1,5 +1,16 @@
 package it.unipv.ingsw.cinema.model.cinema;
 
-public class Room {
+import java.util.ArrayList;
 
+public class Room {
+	
+	private int progressive,seatsNumber;
+	private ArrayList<PhisicalSeat> seats;
+	
+	public Room(int progressive, int seatsNumber) {
+		this.progressive=progressive;
+		this.seatsNumber=seatsNumber;
+		seats=new ArrayList<PhisicalSeat>();
+	}
+	
 }
