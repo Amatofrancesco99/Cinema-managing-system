a263995c4b5d642a8dbf7d44fdf9cde0f8286e70
AmatoFrancesco99 <francesco.amato01@universitadipavia.it>
2021-05-30 13:00:03 +0200

    Added javax.mail.jar to classpath, initial version of sending email via reservation

--- src/main/java/cinema/model/reservation/Reservation.java
+++ src/main/java/cinema/model/reservation/Reservation.java
@@ -2,6 +2,14 @@ package cinema.model.reservation;
 
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.Properties;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 
 import cinema.model.Projection;
 import cinema.model.Spectator;
@@ -56,10 +64,48 @@ public class Reservation {
 	}
 	
 	// invio per email del documento con le informazioni inerenti la reservation
-	public String sendEmail(String email) {
-		if (createReport()!=false) {
-			return "Email spedita";
+	public String sendEmail(String email, String password) {
+		if (createReport()==false) {
+			return "La generazione del report non è andata a buon fine.";
 		}
-		else return "L'invio dell'email non è andato a buon fine";
+		
+		String to = email;
+        String from = "noreply@"+"CinemaArmadillo.com"; //our cinema email
+        // Assuming you are sending email from through gmails smtp
+        String host = "smtp.gmail.com";
+        // Get system properties
+        Properties properties = System.getProperties();
+        // Setup mail server
+        properties.put("mail.smtp.host", host);
+        properties.put("mail.smtp.port", "465");
+        properties.put("mail.smtp.ssl.enable", "true");
+        properties.put("mail.smtp.auth", "true");
+        // Get the Session object and pass username and password
+        Session session = Session.getInstance(properties, new javax.mail.Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(to, password);
+            }
+        });
+        // Used to debug SMTP issues
+        session.setDebug(true);
+        try {
+            // Create a default MimeMessage object.
+            MimeMessage message = new MimeMessage(session);
+            // Set From: header field of the header.
+            message.setFrom(new InternetAddress(from));
+            // Set To: header field of the header.
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+            // Set Subject: header field
+            message.setSubject("RICEZIONE REPORT PRENOTAZIONE ID: " + String.valueOf(progressive));
+            // Now set the actual message
+            message.setText("This is actual message");
+            System.out.println("sending...");
+            // Send message
+            Transport.send(message);
+            return "Sent message successfully....";
+        } catch (MessagingException mex) {
+            mex.printStackTrace();
+        	return "Error in sending email";
+        }
 	}
 }
