b609a3ab763803c0cf027d12c28dfc77d45852a9
Edoardo Vacchini <edoardo.vacchini01@universitadipavia.it>
2021-05-31 23:27:51 +0200

    Refactor on classes

--- src/main/java/cinema/model/Projection.java
+++ src/main/java/cinema/model/Projection.java
@@ -19,20 +19,20 @@ public class Projection {
 	private HashMap<PhisicalSeat,Boolean> availableSeats;
 	
 	public Projection (Date date, String time, Money price) {
-		this.date=date;
-		this.time=time;
-		this.price=price;
+		this.date = date;
+		this.time = time;
+		this.price = price;
 	}
 
 	// setta quale film e dove, il film sarà proiettato
 	public void setMovie (Movie movie) {
-		this.movie=movie;
+		this.movie = movie;
 	}
 	//da testare/controllare conformità
 	public void setRoom (Room room) {
-		this.room=room;
+		this.room = room;
 		availableSeats = new HashMap<PhisicalSeat,Boolean>(room.getNumberSeats());
-		for(int i=0; i<room.getNumberSeats(); i++) 
+		for(int i = 0; i < room.getNumberSeats(); i++) 
 			availableSeats.put(room.getSeat(i), true);	
 	}
 	
