7a9abfb72b7de49ccce4281883cafae128d27cc6
AmatoFrancesco99 <francesco.amato01@universitadipavia.it>
2021-06-01 16:31:26 +0200

    Modified Movie class, in order to have all its useful informations

--- src/main/java/cinema/model/Movie.java
+++ src/main/java/cinema/model/Movie.java
@@ -1,45 +1,100 @@
 package cinema.model;
 
-import java.util.Date;
+import java.time.LocalDate;
+import java.util.ArrayList;
 
 public class Movie {
 
-	private String name,cast,producer;
-	private int duration, minAgeSee;
-	private String urlImage, urlTrailer; 
-	private Date releaseDate;
+	private int id, rating, minAgeSee;
+	private String title, description, duration, imageURL, trailerURL;
+	private ArrayList<String> directors, cast, genres;
+	private LocalDate releaseDate;
 	
-	public Movie (String name, String cast, String producer, int duration, int minAgeSee, String urlImage, String urlTrailer,  Date releaseDate) {
-		this.name = name;
-		this.cast = cast;
-		this.producer = producer;
+	public Movie (int id, String title, String duration, int minAgeSee, LocalDate releaseDate) {
+		this.id = id;
+		this.title = title;
+		this.rating = 0;
 		this.duration = duration;
 		this.minAgeSee = minAgeSee;
 		this.releaseDate = releaseDate;
-		this.urlImage = urlImage;
-		this.urlTrailer = urlTrailer;
+		directors = new ArrayList<String>();
+		cast = new ArrayList<String>();
+		genres = new ArrayList<String>();
 	}
 	
-	// Getters
-	public String getName() {
-		return name;
+	//setters
+	public void setDescription(String description) {
+		this.description = description;
 	}
-	public int getDuration() {
+	
+	public void addDirector(String director) {
+		directors.add(director);
+	}
+	
+	public void addCast(String member) {
+		cast.add(member);
+	}
+	
+	public void addGenres(String genre) {
+		genres.add(genre);
+	}
+	
+	public void setImageURL(String imageURL) {
+		this.imageURL = imageURL;
+	}
+	
+	public void setTrailerURL(String trailerURL) {
+		this.trailerURL = trailerURL;
+	}
+	
+	
+	//getters
+	public int getId() {
+		return id;
+	}
+
+	public int getRating() {
+		return rating;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public String getImageURL() {
+		return imageURL;
+	}
+
+	public String getTrailerURL() {
+		return trailerURL;
+	}
+
+	public String getDuration() {
 		return duration;
 	}
-	public Date getReleaseDate() {
-		return releaseDate;
-	}
-	public String getCast() {
-		return cast;
-	}
-	public String getProducer() {
-		return producer;
-	}
+
 	public int getMinAgeSee() {
 		return minAgeSee;
 	}
-	public String getUrlImage() {
-		return urlImage;
+	
+	public LocalDate getReleaseDate() {
+		return releaseDate;
 	}
+	
+	public ArrayList<String> getDirectors() {
+		return directors;
+	}
+
+	public ArrayList<String> getCast() {
+		return cast;
+	}
+
+	public ArrayList<String> getGenres() {
+		return genres;
+	}
+	
 }
