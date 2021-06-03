079999b5cf131c624343f62133df14fef215634c
AmatoFrancesco99 <francesco.amato01@universitadipavia.it>
2021-06-04 00:16:56 +0200

    Added more important classes to junit - Cinema & Reservation

---
+++ src/main/java/cinema/test/junit/CinemaTest.java
@@ -0,0 +1,19 @@
+package cinema.test.junit;
+
+import static org.junit.Assert.*;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class CinemaTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
