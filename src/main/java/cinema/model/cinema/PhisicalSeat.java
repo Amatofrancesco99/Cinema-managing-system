10405deb384712c8b6c2231161f4fcfb23e9f30c
AmatoFrancesco99 <francesco.amato01@universitadipavia.it>
2021-06-03 21:41:08 +0200

    Add lombok notation to all classes

--- src/main/java/cinema/model/cinema/PhisicalSeat.java
+++ src/main/java/cinema/model/cinema/PhisicalSeat.java
@@ -1,19 +1,16 @@
 package cinema.model.cinema;
 
 import cinema.model.enumerations.TypeOfSeat;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@AllArgsConstructor
 @Data
+@NoArgsConstructor
 public class PhisicalSeat {
 	
 	private char row;
 	private int number;
 	private TypeOfSeat type;
-	
-	public PhisicalSeat(char row, int number, TypeOfSeat type) {
-		this.row = row;
-		this.number = number;
-		this.type = type;
-	}
-	
 }
