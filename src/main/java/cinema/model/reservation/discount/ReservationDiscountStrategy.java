f386e4e6e0294f4b94e5e3ca671cba3a385ec162
AmatoFrancesco99 <francesco.amato01@universitadipavia.it>
2021-06-04 09:01:23 +0200

    Added javadoc, pattern composite & strategy

--- src/main/java/cinema/model/reservation/discount/ReservationDiscountStrategy.java
+++ src/main/java/cinema/model/reservation/discount/ReservationDiscountStrategy.java
@@ -3,6 +3,24 @@ package cinema.model.reservation.discount;
 import cinema.model.Money;
 import cinema.model.reservation.Reservation;
 
+/** BREVE DESCRIZIONE CLASSE ReservationDiscountStrategy (Pattern Strategy + Pattern Composite)
+ * 
+ * @author Screaming Hairy Armadillo Team
+ *
+ * Questa classe, più specificatamente interfaccia, specifica il metodo che deve essere 
+ * implementato da tutte le strategie che applicano sconti sulla prenotazione specifica.
+ * Esistono diversi tipi di sconti che si possono applicare, a seconda delle
+ * politiche del cinema e del direttore di quest ultimo. Chiaramente se ne possono aggiungere
+ * moltissimi altri a piacere. Trovate alcune tra le strategie applicabili sulle prenotazioni
+ * nel sottopackage types, di questo stesso package. 
+ */
 public interface ReservationDiscountStrategy {
+	/**
+	 * METODO per farsi restituire il totale di una prenotazione, dopo che è stato verificato
+	 * se si può calcolare dello sconto su di essa.
+	 * @param r	  	  Prenotazione a cui si vuole verificare se la strategia di sconto è 
+	 * 				  applicabile
+	 * @return Money  Ritorno del prezzo totale scontato
+	 */
 	public Money getTotal(Reservation r);
 }
