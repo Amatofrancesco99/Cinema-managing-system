7521462ac60b7ef57b5085c401342570422a4a5f
AmatoFrancesco99 <francesco.amato01@universitadipavia.it>
2021-06-04 08:50:43 +0200

    Removed useless attributes to PhysicalSeat class, a seat exists even if it is not in a Room

--- src/main/java/cinema/model/cinema/PhysicalSeat.java
+++ src/main/java/cinema/model/cinema/PhysicalSeat.java
@@ -5,12 +5,21 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+/**BREVE SPIEGAZIONE CLASSE PHYSICAL SEAT
+ * 
+ * @author Screaming Hairy Armadillo Team
+ *
+ * La classe rappresenta il posto fisico che è presente all'interno di una specifica 
+ * sala, esistono diversi tipi di posti in un cinema. 
+ * Ovviamente si potrebbero aggiungere molti altri attributi a questa classe, ma per semplicità
+ * e soprattutto per la realtà che vogliamo rappresentare non ci interessa farlo.
+ * Qualora fosse utile per uno specifico cinema aggiungere attributi a questa classe il 
+ * programmatore è libero di aggiungerli.
+ */
 @AllArgsConstructor
 @Data
 @NoArgsConstructor
 public class PhysicalSeat {
 	
-	private char row;
-	private int number;
 	private TypeOfSeat type;
 }
