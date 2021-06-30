2f941342bb812a4ce3745d765dbcc1b6d3661530
Daniele Murer <80338169+daniele-murer@users.noreply.github.com>
2021-06-30 10:19:18 +0200

    Refactor the payment method of GreatNorthernAccountAdapter (now it has a flexible success probability)

--- src/main/java/cinema/model/payment/GreatNorthernAccountingAdapter.java
+++ src/main/java/cinema/model/payment/GreatNorthernAccountingAdapter.java
@@ -4,33 +4,27 @@ import java.util.Random;
 
 import cinema.model.payment.methods.paymentCard.PaymentCard;
 
-public class GreatNorthernAccountingAdapter implements PaymentAdapter{
+public class GreatNorthernAccountingAdapter implements PaymentAdapter {
 
 	@SuppressWarnings("unused")
 	private PaymentCard p;
-	
+
 	public GreatNorthernAccountingAdapter(PaymentCard p) {
 		this.p = p;
 	}
-	
+
 	/**
-	 * METODO per ridurre il saldo della carta
-	 * Resituisce sempre true perché non abbiamo una implementazione vera della carta
-	 * Ipotizziamo semplicemente che qualsiasi cifra inserita venga sempre restituito vero
-	 * Ovviamente bisognerebbe considerare delle problematiche, ossia se per qualche motivo
-	 * non si potesse raggiungere il gestore della carta, o se il saldo da pagare fosse superiore
+	 * METODO per ridurre il saldo della carta Resituisce sempre true perché non
+	 * abbiamo una implementazione vera della carta Ipotizziamo semplicemente che
+	 * qualsiasi cifra inserita venga sempre restituito vero Ovviamente bisognerebbe
+	 * considerare delle problematiche, ossia se per qualche motivo non si potesse
+	 * raggiungere il gestore della carta, o se il saldo da pagare fosse superiore
 	 * rispetto al credito residuo, ecc...
 	 */
 	@Override
 	public boolean pay(double amount) {
-		int mode = 0;
-		switch(mode) {
-			case 0:
-				return true;
-			case 1:
-				return false;
-			default:
-				return new Random().nextBoolean();
-		}
+		double successProbability = 1;
+		return new Random().nextDouble() < successProbability;
 	}
+
 }
