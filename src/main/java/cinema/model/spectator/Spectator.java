0311c759d91aedf324638cf17fca0716c6b81ff4
Daniele Murer <80338169+daniele-murer@users.noreply.github.com>
2021-07-04 15:36:13 +0200

    Add JavaDoc to Spectator and CLIAdminMain classes

--- src/main/java/cinema/model/spectator/Spectator.java
+++ src/main/java/cinema/model/spectator/Spectator.java
@@ -3,7 +3,7 @@ package cinema.model.spectator;
 import cinema.model.spectator.util.InvalidSpectatorInfoException;
 
 /**
- * Rappresenta lo spettatore che effettua la prenotazione della visione del
+ * Rappresenta lo spettatore che effettua la prenotazione della proiezione del
  * film.
  * 
  * @author Screaming Hairy Armadillo Team
@@ -12,45 +12,49 @@ import cinema.model.spectator.util.InvalidSpectatorInfoException;
 public class Spectator {
 
 	/**
-	 * ATTRIBUTI
-	 * 
-	 * @param name    Nome/Nomi
-	 * @param surname Cognome/Cognomi
-	 * @param email   Email
+	 * Nome dello spettatore.
 	 */
-	private String name, surname, email;
+	private String name;
 
 	/**
-	 * COSTRUTTORE della classe
+	 * Cognome dello spettatore.
+	 */
+	private String surname;
+
+	/**
+	 * E-mail dello spettatore.
+	 */
+	private String email;
+
+	/**
+	 * Costruttore dello spettatore.
 	 * 
-	 * @param name
-	 * @param surname
-	 * @param email
-	 * @throws InvalidSpectatorInfoException
+	 * @param name    nome dello spettatore.
+	 * @param surname cognome dello spettatore.
+	 * @param email   e-mail dello spettatore.
+	 * @throws InvalidSpectatorInfoException se almeno uno dei parametri è una
+	 *                                       stringa vuota.
 	 */
 	public Spectator(String name, String surname, String email) throws InvalidSpectatorInfoException {
 		if ((email.equals("")) || (name.equals("")) || (surname.equals(""))) {
 			throw new InvalidSpectatorInfoException("I dati personali richiesti sono mancanti.");
 		} else {
 			this.name = name;
-			;
 			this.surname = surname;
 			this.email = email;
 		}
 	}
 
-	/** METODO per farsi dire il nome dello spettatore */
 	public String getName() {
 		return name;
 	}
 
-	/** METODO per farsi dire il cognome dello spettatore */
 	public String getSurname() {
 		return surname;
 	}
 
-	/** METODO per farsi dire l'email dello spettatore */
 	public String getEmail() {
 		return email;
 	}
+
 }
