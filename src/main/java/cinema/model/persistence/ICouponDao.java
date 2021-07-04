3063705eb519bfb2df36f59a56fc60a6d7d78d0a
Edoardo Vacchini <edoardo.vacchini01@universitadipavia.it>
2021-07-04 23:04:23 +0200

    Add Javadoc on ICouponDao, IDiscountDao, PersistenceException, IPaymentAdapter

--- src/main/java/cinema/model/persistence/ICouponDao.java
+++ src/main/java/cinema/model/persistence/ICouponDao.java
@@ -6,8 +6,43 @@ import java.util.ArrayList;
 import cinema.model.reservation.discount.coupon.Coupon;
 import cinema.model.reservation.discount.coupon.util.CouponException;
 
+/**
+ * Contiene i metodi necessari per mantenere la persistenza dei dati riguardanti
+ * i coupon.
+ * 
+ * @author Screaming Hairy Armadillo Team
+ *
+ */
 public interface ICouponDao {
+	/**
+	 * Restituisce tutti i coupon presenti nel database.
+	 * 
+	 * @return i coupon presenti nel database.
+	 * @throws SQLException    se vengono riscontrati errori nell'interazione con il
+	 *                         meccanismo di persistenza.
+	 * @throws CouponException se il codice del coupon non è valido, ovvero non di
+	 *                         almeno otto caratteri.
+	 */
 	public ArrayList<Coupon> getAllCoupons() throws SQLException, CouponException;
+
+	/**
+	 * Restituisce un coupon sulla base del suo codice promozionale.
+	 * 
+	 * @param promoCode codice promozionale del coupon da restituire.
+	 * @return un coupon sulla base del proprio codice promozionale.
+	 * @throws SQLException    se vengono riscontrati errori nell'interazione con il
+	 *                         meccanismo di persistenza.
+	 * @throws CouponException se il codice del coupon identificato non è valido,
+	 *                         ovvero non è di almeno otto caratteri.
+	 */
 	public Coupon getCoupon(String promoCode) throws SQLException, CouponException;
+
+	/**
+	 * Imposta un coupon come usato attraverso il meccanismo di persistenza.
+	 * 
+	 * @param promoCode codice promozionale del coupon da segnalare come utilizzato.
+	 * @throws SQLException se vengono riscontrati errori nell'interazione con il
+	 *                      meccanismo di persistenza.
+	 */
 	public void setCouponUsed(String promoCode) throws SQLException;
 }
--- src/main/java/cinema/model/persistence/IDiscountDao.java
+++ src/main/java/cinema/model/persistence/IDiscountDao.java
@@ -6,8 +6,44 @@ import cinema.model.reservation.discount.types.DiscountAge;
 import cinema.model.reservation.discount.types.DiscountDay;
 import cinema.model.reservation.discount.types.DiscountNumberSpectators;
 
+/**
+ * Contiene i metodi necessari per mantenere la persistenza dei dati riguardanti
+ * gli sconti.
+ * 
+ * @author Screaming Hairy Armadillo Team
+ *
+ */
 public interface IDiscountDao {
+	/**
+	 * Restituisce tutti gli sconti che applicano una riduzione di prezzo in base
+	 * alla data di proiezione presenti nel database.
+	 * 
+	 * @return gli sconti presenti nel database che applicanola riduzione di prezzo
+	 *         sulla base della data di proiezione.
+	 * @throws SQLException se vengono riscontrati errori nell'interazione con il
+	 *                      meccanismo di persistenza.
+	 */
 	public DiscountDay getAllDayDiscounts() throws SQLException;
+
+	/**
+	 * Restituisce tutti gli sconti che applicano una riduzione di prezzo in base
+	 * all'età dello spettaore.
+	 * 
+	 * @return gli sconti presenti nel database che applicanola riduzione di prezzo
+	 *         sulla base dell'età dello spettatore.
+	 * @throws SQLException se vengono riscontrati errori nell'interazione con il
+	 *                      meccanismo di persistenza.
+	 */
 	public DiscountAge getAgeDiscounts() throws SQLException;
+
+	/**
+	 * Restituisce tutti gli sconti che applicano una riduzione di prezzo in base al
+	 * nuemero di biglietti comprati.
+	 * 
+	 * @return gli sconti presenti nel database che applicanola riduzione di prezzo
+	 *         sulla base della al nuemero di biglietti comprati.
+	 * @throws SQLException se vengono riscontrati errori nell'interazione con il
+	 *                      meccanismo di persistenza.
+	 */
 	public DiscountNumberSpectators getGroupDiscounts() throws SQLException;
 }
