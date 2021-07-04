ea2f8a42286971260afb7dce119b34b5e427a4a1
Edoardo Vacchini <edoardo.vacchini01@universitadipavia.it>
2021-07-05 01:52:33 +0200

    Add javaDoc in CinemaRdbDao, CouponRdbDao, DiscountRdbDao, MovieRdbDao, RoomRdbDao

--- src/main/java/cinema/model/persistence/DiscountRdbDao.java
+++ src/main/java/cinema/model/persistence/DiscountRdbDao.java
@@ -11,39 +11,71 @@ import cinema.model.reservation.discount.types.DiscountAge;
 import cinema.model.reservation.discount.types.DiscountDay;
 import cinema.model.reservation.discount.types.DiscountNumberSpectators;
 
-public class DiscountRdbDao implements IDiscountDao{
+/**
+ * Si interfaccia con un database relazionale per implementare la persistenza
+ * dei dati degli sconti gestiti dall'applicazione.
+ * 
+ * @author Screaming Hairy Armadillo Team
+ *
+ */
+public class DiscountRdbDao implements IDiscountDao {
+	/**
+	 * Connessione al database.
+	 */
 	private Connection connection;
 
+	/**
+	 * Costruttore dell'interfaccia verso il database relazionale.
+	 * 
+	 * @param connection connessione al database relazionale che impelemta la
+	 *                   persistenza delle informazioni.
+	 */
 	public DiscountRdbDao(Connection connection) {
 		this.connection = connection;
 	}
-	
+
+	/**
+	 * Esegue la query per recuperare tutti gli sconti basati sul giorno della
+	 * proiezione presenti sul database relazionale.
+	 */
 	@Override
 	public DiscountDay getAllDayDiscounts() throws SQLException {
 		String sql = "SELECT * FROM Discount WHERE type = \"DAY\";";
-        PreparedStatement pstatement  = connection.prepareStatement(sql);
-        ResultSet result = pstatement.executeQuery();
-        DiscountDay discounts = new DiscountDay(result.getInt("id"));
-        while (result.next()) {
-        	discounts.addDayDiscount(LocalDate.parse(result.getString("date"), DateTimeFormatter.ofPattern("yyyy-MM-dd")), result.getDouble("percentage"));
-        }
-        return discounts;
+		PreparedStatement pstatement = connection.prepareStatement(sql);
+		ResultSet result = pstatement.executeQuery();
+		DiscountDay discounts = new DiscountDay(result.getInt("id"));
+		while (result.next()) {
+			discounts.addDayDiscount(
+					LocalDate.parse(result.getString("date"), DateTimeFormatter.ofPattern("yyyy-MM-dd")),
+					result.getDouble("percentage"));
+		}
+		return discounts;
 	}
 
+	/**
+	 * Esegue la query per recuperare tutti gli sconti basati sull'età dello
+	 * spettatore presenti sul database relazionale.
+	 */
 	@Override
 	public DiscountAge getAgeDiscounts() throws SQLException {
 		String sql = "SELECT * FROM Discount WHERE type = \"AGE\";";
-        PreparedStatement pstatement  = connection.prepareStatement(sql);
-        ResultSet result = pstatement.executeQuery();
-        return new DiscountAge(result.getInt("minage"), result.getInt("maxage"), result.getDouble("percentage"), result.getInt("id"));
+		PreparedStatement pstatement = connection.prepareStatement(sql);
+		ResultSet result = pstatement.executeQuery();
+		return new DiscountAge(result.getInt("minage"), result.getInt("maxage"), result.getDouble("percentage"),
+				result.getInt("id"));
 	}
 
+	/**
+	 * Esegue la query per recuperare tutti gli sconti basati sul numero di posti
+	 * prenotati presenti sul database relazionale.
+	 */
 	@Override
 	public DiscountNumberSpectators getGroupDiscounts() throws SQLException {
 		String sql = "SELECT * FROM Discount WHERE type = \"NUMBER\";";
-        PreparedStatement pstatement  = connection.prepareStatement(sql);
-        ResultSet result = pstatement.executeQuery();
-        return new DiscountNumberSpectators(result.getInt("numberpeople"), result.getDouble("percentage"), result.getInt("id"));
+		PreparedStatement pstatement = connection.prepareStatement(sql);
+		ResultSet result = pstatement.executeQuery();
+		return new DiscountNumberSpectators(result.getInt("numberpeople"), result.getDouble("percentage"),
+				result.getInt("id"));
 	}
 
 }
--- src/main/java/cinema/model/persistence/MovieRdbDao.java
+++ src/main/java/cinema/model/persistence/MovieRdbDao.java
@@ -9,39 +9,68 @@ import java.util.Arrays;
 
 import cinema.model.Movie;
 
-public class MovieRdbDao implements IMovieDao{
+/**
+ * Si interfaccia con un database relazionale per implementare la persistenza
+ * dei dati dei film gestiti dall'applicazione.
+ * 
+ * @author Screaming Hairy Armadillo Team
+ *
+ */
+public class MovieRdbDao implements IMovieDao {
+	/**
+	 * Connessione al database.
+	 */
 	private Connection connection;
-	
+
+	/**
+	 * Costruttore dell'interfaccia verso il database relazionale.
+	 * 
+	 * @param connection connessione al database relazionale che impelemta la
+	 *                   persistenza delle informazioni.
+	 */
 	public MovieRdbDao(Connection connection) {
 		this.connection = connection;
 	}
-	
+
+	/**
+	 * Esegue la query al database relazionale per recuperare le informazioni sul
+	 * film identificato da {@code id}.
+	 */
 	@Override
 	public Movie getMovie(int id) throws SQLException {
 		String sql = "SELECT * FROM Movie WHERE id = ?;";
-        PreparedStatement pstatement  = connection.prepareStatement(sql);
-        pstatement.setInt(1, id);
-        ResultSet result = pstatement.executeQuery();
-        ArrayList<String> genres = new ArrayList<String>( Arrays.asList( result.getString("genres").split(",") ));
-        ArrayList<String> cast = new ArrayList<String>( Arrays.asList( result.getString("cast").split(",") ));
-        ArrayList<String> directors = new ArrayList<String>( Arrays.asList( result.getString("directors").split(",") ));
-        Movie movie = new Movie(id, result.getString("title"), result.getString("description"), genres, directors, cast, result.getInt("rating"), result.getInt("duration"), result.getString("imageurl"), result.getString("trailerurl"));
+		PreparedStatement pstatement = connection.prepareStatement(sql);
+		pstatement.setInt(1, id);
+		ResultSet result = pstatement.executeQuery();
+		ArrayList<String> genres = new ArrayList<String>(Arrays.asList(result.getString("genres").split(",")));
+		ArrayList<String> cast = new ArrayList<String>(Arrays.asList(result.getString("cast").split(",")));
+		ArrayList<String> directors = new ArrayList<String>(Arrays.asList(result.getString("directors").split(",")));
+		Movie movie = new Movie(id, result.getString("title"), result.getString("description"), genres, directors, cast,
+				result.getInt("rating"), result.getInt("duration"), result.getString("imageurl"),
+				result.getString("trailerurl"));
 		return movie;
 	}
 
+	/**
+	 * Esegue la query per recuperare tutti i film presenti sul database
+	 * relazionale.
+	 */
 	@Override
 	public ArrayList<Movie> getAllMovies() throws SQLException {
 		String sql = "SELECT * FROM Movie;";
-        PreparedStatement pstatement  = connection.prepareStatement(sql);
-        ResultSet result = pstatement.executeQuery();
-        ArrayList<Movie> movies = new ArrayList<Movie>();
-        while (result.next()) {
-        	ArrayList<String> genres = new ArrayList<String>( Arrays.asList( result.getString("genres").split(",") ));
-            ArrayList<String> cast = new ArrayList<String>( Arrays.asList( result.getString("cast").split(",") ));
-            ArrayList<String> directors = new ArrayList<String>( Arrays.asList( result.getString("directors").split(",") ));
-            movies.add(new Movie(result.getInt("id"), result.getString("title"), result.getString("description"), genres, directors, cast, result.getInt("rating"), result.getInt("duration"), result.getString("imageurl"), result.getString("trailerurl")));
-        }
-        return movies;
+		PreparedStatement pstatement = connection.prepareStatement(sql);
+		ResultSet result = pstatement.executeQuery();
+		ArrayList<Movie> movies = new ArrayList<Movie>();
+		while (result.next()) {
+			ArrayList<String> genres = new ArrayList<String>(Arrays.asList(result.getString("genres").split(",")));
+			ArrayList<String> cast = new ArrayList<String>(Arrays.asList(result.getString("cast").split(",")));
+			ArrayList<String> directors = new ArrayList<String>(
+					Arrays.asList(result.getString("directors").split(",")));
+			movies.add(new Movie(result.getInt("id"), result.getString("title"), result.getString("description"),
+					genres, directors, cast, result.getInt("rating"), result.getInt("duration"),
+					result.getString("imageurl"), result.getString("trailerurl")));
+		}
+		return movies;
 	}
 
 }
